6eae190cbd8bd411d0c748726a10fa1830ecd755
KirillArhipov <karhipov@truvisibility.com>
2024-02-17 15:59:12 +0300

    add eslint, fix command

--- monopoly.Server/obj/Debug/net8.0/monopoly.Server.MvcApplicationPartsAssemblyInfo.cs
+++ monopoly.Server/obj/Debug/net8.0/monopoly.Server.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("monopoly.Server")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0f0b9f54e23ab9ef359da27d03b84dd3f9a77e9")]
-[assembly: System.Reflection.AssemblyProductAttribute("monopoly.Server")]
-[assembly: System.Reflection.AssemblyTitleAttribute("monopoly.Server")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
